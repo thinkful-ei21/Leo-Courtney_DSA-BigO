e580439eb62e8af7aa45b00a936b07e540e5bf95
Courtney Adams <cadams8238@gmail.com>
2018-07-24 08:28:52 -0700

    added outline of first problem, even or odd

---
+++ bigO.js
@@ -0,0 +1,14 @@
+// EVEN OR ODD___________________________________
+
+function isEven(value){
+  if (value % 2 == 0){
+    return true;
+  }
+  else
+    return false;
+}
+
+// Notation:
+
+// Why:
+//
